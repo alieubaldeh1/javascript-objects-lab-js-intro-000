79137014946f6fc8e8f983c5c45773623723616c
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-11 20:27:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function updateObjectWithKeyandValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
+  Object.assign({}, object, {[key]: value})
+  return object
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key) {
-
+  Object.assign({}, object, {delete [key]: value})
+  return object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-
+  delete object[key]
+  return object
 }
