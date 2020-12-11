5a3a3bba77ec3b752d48d4ed2e480bf48bbb1a4b
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-11 20:04:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function updateObjectWithKeyandValue(object, key, value) { 
+  object[key] = value
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
